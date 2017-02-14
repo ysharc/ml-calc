c1852c228dd97318824c10a8bd49a33c2f9fecbf
Yeshwanth Arcot <ysharc@gmail.com>
2017-02-14 22:26:30 +0530

    Completed metrics script

--- metrics.py
+++ metrics.py
@@ -6,11 +6,30 @@
 import os
 import model
 import pre_process
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 	AVAILABLE_METRICS = os.listdir("metrics/")
 	for mod in model.MODELS:
 		if mod not in AVAILABLE_METRICS:
+			os.makedirs("metrics/"+mod)
+			print("loading model")
 			classifier = model.load(mod)
+			print("loading test data")
 			test_data, labels = pre_process.create_test_data()
-			predicted = classifier.predict(test_data)
+			print("predicting test data")
+			predicted = classifier.predict(test_data.values)
+			print("creating metrics")
+			actual_vs_predicted = {i: {} for i in range(10)}
+			for i in range(len(labels)):
+				if predicted[i] in actual_vs_predicted[labels[i]]:
+					actual_vs_predicted[labels[i]][predicted[i]] += 1
+				else:
+					actual_vs_predicted[labels[i]][predicted[i]] = 1
+			print(accuracy_score(labels, predicted))
+			print("saving images")
+			for number in actual_vs_predicted:
+				plt.bar(list(actual_vs_predicted[number].keys()), list(actual_vs_predicted[number].values()))
+				plt.savefig("metrics/" + mod + "/" + str(number)+".png")
+				plt.gcf().clear()
--- model.py
+++ model.py
@@ -13,10 +13,10 @@ def load(model_name, parameters=None):
 		if model is not already available in the models, it is trained and returned
 	"""
 	if model_name + '.pkl' in os.listdir('models') and not parameters:
-		return joblib.load(model_name + '.pkl')
+		return joblib.load('models/' + model_name + '.pkl')
 
 	model = MODELS[model_name]()
-	joblib.dump(model, model_name + ".pkl")
+	joblib.dump(model, 'models/' +  model_name + ".pkl")
 	return model
 
 def knn_default():
